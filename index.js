30446659dcd6a6cbe54d5bebe7e347acf7ad448b
Alexei Tsigulin <mail@neolp.com>
2019-09-12 17:41:26 +0700

    add subscribeOnce to subscribe for mobx

--- index.js
+++ index.js
@@ -13,205 +13,249 @@ var COMMAND = {
   UNSUBSCRIBE: 18
 };
 
-function ITMP(settings) {
-  var defaultSettings = {
-    uri: "ws://" + window.location.host + "/ws/",
-    binaryType: null,
-    autoReconnect: false,
-    reconnectTimeout: 3000,
-    reconnectMaxCount: 0,
-    onOpen: function () { },
-    onClose: function () { },
-    onError: function () { },
-    onReconnect: function () { }
+class ITMP {
+  constructor(settings) {
+    var defaultSettings = {
+      uri: "ws://" + window.location.host + "/ws/",
+      binaryType: null,
+      autoReconnect: false,
+      reconnectTimeout: 3000,
+      reconnectMaxCount: 0,
+      onOpen: function () { },
+      onClose: function () { },
+      onError: function () { },
+      onReconnect: function () { }
+    };
+
+    this.settings = Object.assign({}, defaultSettings, settings);
+    this.reset();
+  }
+
+  reset() {
+    this.state = WebSocket.CLOSED;
+    this.reqIdCount = 0;
+    this.reconnectCount = 0;
+    this._messageQueue = [];
+    this._calls = {};
+    this._subs = {};
   };
 
-  this.settings = Object.assign({}, defaultSettings, settings);
-  this.reset();
-}
+  connect() {
+    this._ws = new WebSocket(this.settings.uri);
+    if (this.settings.binaryType) {
+      this._ws.binaryType = this.settings.binaryType;
+    }
+    // TODO
+    this.state = this._ws.readyState;
+    this._ws.onopen = this._onOpen.bind(this);
+    this._ws.onclose = this._onClose.bind(this);
+    this._ws.onmessage = this._onMessage.bind(this);
+    this._ws.onerror = this._onError.bind(this);
+  };
 
-ITMP.prototype.reset = function () {
-  this.state = WebSocket.CLOSED;
-  this.reqIdCount = 0;
-  this.reconnectCount = 0;
-  this._messageQueue = [];
-  this._calls = {};
-  this._subs = {};
-};
-
-ITMP.prototype.connect = function () {
-  this._ws = new WebSocket(this.settings.uri);
-  if (this.settings.binaryType) {
-    this._ws.binaryType = this.settings.binaryType;
-  }
-  // TODO
-  this.state = this._ws.readyState;
-  this._ws.onopen = this._onOpen.bind(this);
-  this._ws.onclose = this._onClose.bind(this);
-  this._ws.onmessage = this._onMessage.bind(this);
-  this._ws.onerror = this._onError.bind(this);
-};
-
-ITMP.prototype.call = function (topic, args) {
-  return new Promise((resolve, reject) => {
-    var reqId = this._getReqId();
-    this._calls[reqId] = { onSuccess: resolve, onError: reject };
-    this._send([COMMAND.CALL, reqId, topic, args]);
-  })
-
-};
-
-// подписка на топик, onEvent может быть undefined или даже null, или быть опущеным но тогда подписка произойдет но call back вызван не будет
-ITMP.prototype.subscribe = function (topic, onEvent) {
-  return new Promise((resolve, reject) => {
-
-    if (this._subs[topic] === undefined) {
+  call(topic, args) {
+    return new Promise((resolve, reject) => {
       var reqId = this._getReqId();
-      this._subs[topic] = new Map()
-      this._subs[topic].set(onEvent, { onEvent, count: 1 })
       this._calls[reqId] = { onSuccess: resolve, onError: reject };
+      this._send([COMMAND.CALL, reqId, topic, args]);
+    })
+
+  };
+
+  // подписка на топик, onEvent может быть undefined или даже null, или быть опущеным но тогда подписка произойдет но call back вызван не будет
+  subscribe(topic, onEvent) {
+    return new Promise((resolve, reject) => {
+
+      if (this._subs[topic] === undefined) {
+        var reqId = this._getReqId();
+        this._subs[topic] = new Map()
+        this._subs[topic].set(onEvent, { onEvent, count: 1 })
+        this._calls[reqId] = { onSuccess: resolve, onError: reject };
+        this._send([COMMAND.SUBSCRIBE, reqId, topic]);
+        return
+      }
+
+      // Подписка уже есть
+      // просто отправим call ?
+      // this.call(s);
+      if (this._subs[topic].has(onEvent)) {
+        this._subs[topic].get(onEvent).count += 1;
+      } else {
+        this._subs[topic].set(onEvent, { onEvent, count: 1 });
+      }
+    })
+  };
+
+  subscribeOnce(topic, onEvent) {
+    return new Promise((resolve, reject) => {
+
+      if (this._subs[topic] === undefined) {
+        var reqId = this._getReqId();
+        this._subs[topic] = new Map()
+        this._subs[topic].set(null, { onEvent, count: 1 })
+        this._calls[reqId] = { onSuccess: resolve, onError: reject };
+        this._send([COMMAND.SUBSCRIBE, reqId, topic]);
+        return
+      }
+
+      // Подписка уже есть
+      // просто отправим call ?
+      // this.call(s);
+      if (this._subs[topic].has(null)) {
+        this._subs[topic].get(null).count += 1;
+      } else {
+        this._subs[topic].set(null, { onEvent, count: 1 });
+      }
+    })
+  };
+
+  _resubscribeAll() {
+    for (var topic in this._subs) {
+      var reqId = this._getReqId();
+      this._calls[reqId] = { onSuccess: () => { }, onError: () => { } };
       this._send([COMMAND.SUBSCRIBE, reqId, topic]);
-      return
     }
+  };
 
-    // Подписка уже есть
-    // просто отправим call ?
-    // this.call(s);
-    if (this._subs[topic].has(onEvent)) {
-      this._subs[topic].get(onEvent).count += 1;
-    } else {
-      this._subs[topic].set(onEvent, { onEvent, count: 1 });
-    }
-  })
-};
+  // отписка от топика, onEvent может быть undefined или даже null, или быть опущеным, главное чтобы также как и при вызове subscribe
+  unsubscribe(topic, onEvent) {
+    return new Promise((resolve, reject) => {
+      if (this._subs[topic] && this._subs[topic].has(onEvent)) {
+        this._subs[topic].get(onEvent).count -= 1;
 
-ITMP.prototype._resubscribeAll = function () {
-  for (var topic in this._subs) {
-    var reqId = this._getReqId();
-    this._calls[reqId] = { onSuccess: () => { }, onError: () => { } };
-    this._send([COMMAND.SUBSCRIBE, reqId, topic]);
-  }
-};
+        if (this._subs[topic].get(onEvent).count === 0) {
+          this._subs[topic].delete(onEvent);
 
-// отписка от топика, onEvent может быть undefined или даже null, или быть опущеным, главное чтобы также как и при вызове subscribe
-ITMP.prototype.unsubscribe = function (topic, onEvent) {
-  return new Promise((resolve, reject) => {
-    if (this._subs[topic] && this._subs[topic].has(onEvent)) {
-      this._subs[topic].get(onEvent).count -= 1;
+          if (this._subs[topic].size === 0) {
+            delete this._subs[topic];
 
-      if (this._subs[topic].get(onEvent).count === 0) {
-        this._subs[topic].delete(onEvent);
-
-        if (this._subs[topic].size === 0) {
-          delete this._subs[topic];
-
-          var reqId = this._getReqId();
-          this._calls[reqId] = { onSuccess: resolve, onError: reject };
-          this._send([COMMAND.UNSUBSCRIBE, reqId, topic]);
+            var reqId = this._getReqId();
+            this._calls[reqId] = { onSuccess: resolve, onError: reject };
+            this._send([COMMAND.UNSUBSCRIBE, reqId, topic]);
+          }
         }
       }
+    })
+  };
+
+  unsubscribeOnce(topic) {
+    return new Promise((resolve, reject) => {
+      if (this._subs[topic] && this._subs[topic].has(null)) {
+        this._subs[topic].get(null).count -= 1;
+
+        if (this._subs[topic].get(null).count === 0) {
+          this._subs[topic].delete(null);
+
+          if (this._subs[topic].size === 0) {
+            delete this._subs[topic];
+
+            var reqId = this._getReqId();
+            this._calls[reqId] = { onSuccess: resolve, onError: reject };
+            this._send([COMMAND.UNSUBSCRIBE, reqId, topic]);
+          }
+        }
+      }
+    })
+  };
+
+  _onOpen(evt) {
+    console.log("ws open");
+    this.state = this._ws.readyState;
+    this._send(); // отправиль все сообщения из очереди
+
+    if (this.reconnectCount) {
+      this._resubscribeAll();
+      this.reconnectCount = 0;
     }
-  })
-};
 
-ITMP.prototype._onOpen = function (evt) {
-  console.log("ws open");
-  this.state = this._ws.readyState;
-  this._send(); // отправиль все сообщения из очереди
+    this.settings.onOpen(evt);
+  };
 
-  if (this.reconnectCount) {
-    this._resubscribeAll();
-    this.reconnectCount = 0;
-  }
-
-  this.settings.onOpen(evt);
-};
-
-ITMP.prototype._onClose = function (evt) {
-  this.settings.onClose(evt);
-  this.state = this._ws.readyState;
-  this._ws = null;
-  if (this.settings.autoReconnect && (!this.settings.reconnectMaxCount || this.reconnectCount < this.settings.reconnectMaxCount)) {
-    //TODO if force close
-    setTimeout(this._onReconnect.bind(this), this.settings.reconnectTimeout);
-  }
-};
-
-ITMP.prototype._onMessage = function (evt) {
-  try {
-    var msg = this._unserialize(evt.data);
-  } catch (e) {
-    console.error('Unserialize ITMP error', evt.data)
-    return;
-  }
-  var code = msg[0],
-    reqId = msg[1],
-    topic = msg[2],
-    payload = msg[3];
-
-  if (code === COMMAND.ERROR) {
-    this._calls[reqId] && this._calls[reqId].onError && this._calls[reqId].onError(payload);
-    delete this._calls[reqId];
-    return;
-  }
-
-  if (code === COMMAND.EVENT) {
-    if (this._subs[topic]) {
-      this._subs[topic].forEach(function (c) {
-        c && c.onEvent && c.onEvent(topic, payload);
-      });
+  _onClose(evt) {
+    this.settings.onClose(evt);
+    this.state = this._ws.readyState;
+    this._ws = null;
+    if (this.settings.autoReconnect && (!this.settings.reconnectMaxCount || this.reconnectCount < this.settings.reconnectMaxCount)) {
+      //TODO if force close
+      setTimeout(this._onReconnect.bind(this), this.settings.reconnectTimeout);
     }
-  } else {
-    this._calls[reqId] && this._calls[reqId].onSuccess && this._calls[reqId].onSuccess(topic, payload); // it is answer and "payload" ocupied "topic" place
-    delete this._calls[reqId];
-  }
-};
+  };
 
-ITMP.prototype._onError = function (evt) {
-  this.settings.onError(evt);
-};
-
-ITMP.prototype._onReconnect = function () {
-  if (this._ws && this._ws.readyState !== WebSocket.CLOSED) {
-    return;
-  }
-
-  this.reconnectCount += 1;
-  this.settings.onReconnect();
-  this.connect();
-};
-
-ITMP.prototype._send = function (message) {
-  if (message) {
-    this._messageQueue.push(this._serialize(message));
-  }
-
-  if (this._ws && this._ws.readyState === WebSocket.OPEN) {
-    while (this._messageQueue.length) {
-      this._ws.send(this._messageQueue.shift());
+  _onMessage(evt) {
+    try {
+      var msg = this._unserialize(evt.data);
+    } catch (e) {
+      console.error('Unserialize ITMP error', evt.data)
+      return;
     }
-  }
-};
+    var code = msg[0],
+      reqId = msg[1],
+      topic = msg[2],
+      payload = msg[3];
+
+    if (code === COMMAND.ERROR) {
+      this._calls[reqId] && this._calls[reqId].onError && this._calls[reqId].onError(payload);
+      delete this._calls[reqId];
+      return;
+    }
+
+    if (code === COMMAND.EVENT) {
+      if (this._subs[topic]) {
+        this._subs[topic].forEach(function (c) {
+          c && c.onEvent && c.onEvent(topic, payload);
+        });
+      }
+    } else {
+      this._calls[reqId] && this._calls[reqId].onSuccess && this._calls[reqId].onSuccess(topic, payload); // it is answer and "payload" ocupied "topic" place
+      delete this._calls[reqId];
+    }
+  };
+
+  _onError(evt) {
+    this.settings.onError(evt);
+  };
+
+  _onReconnect() {
+    if (this._ws && this._ws.readyState !== WebSocket.CLOSED) {
+      return;
+    }
+
+    this.reconnectCount += 1;
+    this.settings.onReconnect();
+    this.connect();
+  };
+
+  _send(message) {
+    if (message) {
+      this._messageQueue.push(this._serialize(message));
+    }
+
+    if (this._ws && this._ws.readyState === WebSocket.OPEN) {
+      while (this._messageQueue.length) {
+        this._ws.send(this._messageQueue.shift());
+      }
+    }
+  };
 
 
 
 
-ITMP.prototype._getReqId = function () {
-  return ++this.reqIdCount;
-};
+  _getReqId() {
+    return ++this.reqIdCount;
+  };
 
-ITMP.prototype._serialize = function (msg) {
-  if (this.settings.binaryType) {
-    return CBOR.encode(msg);
-  }
-  return JSON.stringify(msg);
-};
+  _serialize(msg) {
+    if (this.settings.binaryType) {
+      return CBOR.encode(msg);
+    }
+    return JSON.stringify(msg);
+  };
 
-ITMP.prototype._unserialize = function (msg) {
-  if (this.settings.binaryType) {
-    return CBOR.decode(msg);
-  }
-  return JSON.parse(msg);
-};
-
-export default new ITMP()
+  _unserialize(msg) {
+    if (this.settings.binaryType) {
+      return CBOR.decode(msg);
+    }
+    return JSON.parse(msg);
+  };
+}
+export default ITMP
